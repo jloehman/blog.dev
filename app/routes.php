ad42666d45198cf3d955542898a95d1bb0ae22ff
JLo <jkloehman@Jasons-MacBook-Air.local>
2014-07-01 11:03:57 -0500

    Updated routes for blog

--- app/routes.php
+++ app/routes.php
@@ -13,19 +13,11 @@
 
 // rolldice example
 
+Route::get('/', 'HomeController@showWelcome');
+
 Route::get('/rolldice/{guess}', function($guess)
 {
 
-	// if(! is_numeric($guess))
-	// {
-	// 	return Redirect::to('/rolldice');
-	// }
-	// if ($guess == 'random')
- //    {
- //        return Redirect::to('/');
- //    }
- //    else
- //    {
 	$random = rand(1,6);
 	$data = array
 	(
@@ -39,15 +31,64 @@ Route::get('/rolldice/{guess}', function($guess)
 
 //resume and portfolio blog
 
-Route::get('/sayhello/{name}', function($name)
+//Route::get('/sayhello/{name}', 'HomeController@sayHello');
+
+Route::resource('posts', 'PostsController');
+
+
+
+// portfolio and resume blog
+
+Route::get('/', function()
 {
-    if ($name == "Jason")
-    {
-        return Redirect::to('/');
-    }
-    else
-    {
-        $data = array('name' => $name);
-        return View::make('my-first-view')->with($data);
-    }
+	return View::make('layouts.master');
 });
+
+route::resource('posts', 'PostsController');
+
+Route::get('/resume/{}', function()
+{
+	return View::make('resume');
+});
+
+
+Route::get('/portfolio/{}', function()
+{
+	return View::make('portfolio');
+});
+
+
+Route::get('/orm-test', function()
+{
+	// $post = new Post();
+	// $post->title = "New Blog Post";
+	// $post->body = "Holy crap writing code using eloquent ORM is easy like Andrew's mom!";
+	// $post->save();
+
+	// $post = new Post();
+	// $post->title = "Even more blog posts";
+	// $post->body = "this is not soo bad";
+	// $post->save();
+
+	// $posts = Post::all();
+		// foreach ($posts as $post){
+		// 	echo $post->title . "<br>";
+		// 	echo $post->body . "<br>";
+
+		// }
+		
+
+	// $post = Post::find(1);
+
+	// echo $post->title . "<br>";
+	// echo $post->body . "<br>";
+
+	// $post = Post::delete();
+
+	// $post->title = "This is a NEW title";
+
+	// return "Eloquent ORM is Eloquent";
+
+	// print_r($post);
+});
+?>
